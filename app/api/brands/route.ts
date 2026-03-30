0603420241729a7b923ef82bec97b391b7fec76a
adam <adamfreeman2024@gmail.com>
2026-03-31 01:36:09 +0400

    fix: added ISR caching to api routes to prevent server overload

--- app/api/brands/route.ts
+++ app/api/brands/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from "@/lib/prisma";
-export const dynamic = 'force-dynamic';
+export const revalidate = 3600;
 
 
 // Эту функцию вызывает Главная страница, чтобы показать бегущую строку
